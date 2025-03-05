ccd8e84ae283406598765a6107dee3f82262c0d6
Federico Javier <145453141+Feder1711@users.noreply.github.com>
2025-03-05 09:40:41 +0100

    Subiendo código actualizado de la práctica 3

--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -1,32 +1,25 @@
 package aplicacion;
 
 import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class Matematicas {
 
-    /**
-     * Genera una aproximación al número Pi mediante el método de Montecarlo.
-     * 
-     * @param pasos El número de puntos generados para la aproximación de Pi.
-     * @return Una aproximación del valor de Pi.
-     */
     public static double generarNumeroPi(int pasos) {
-        int dentroCirculo = 0;
-
         Random rand = new Random();
-        for (int i = 0; i < pasos; i++) {
-            double x = rand.nextDouble(); // Coordenada x aleatoria entre 0 y 1
-            double y = rand.nextDouble(); // Coordenada y aleatoria entre 0 y 1
 
-            double distancia = Math.sqrt(x * x + y * y);
+        Supplier<Double> puntoAleatorio = () -> {
+            double x = rand.nextDouble();
+            double y = rand.nextDouble();
+            return Math.sqrt(x * x + y * y);
+        };
 
-            if (distancia <= 1) {
-                dentroCirculo++;
-            }
-        }
+        long dentroCirculo = IntStream.range(0, pasos)
+                .mapToObj(i -> puntoAleatorio.get())
+                .filter(distancia -> distancia <= 1)
+                .count();
 
-        // Aproximación al número pi
         return 4.0 * dentroCirculo / pasos;
     }
 }
-
